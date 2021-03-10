06878bddec502046d6561e06c6517ba1c0ae4e39
orchsik <orchsik@naver.com>
2021-03-10 12:42:33 +0900

    Add: HelloAdapterView

--- HelloAdapterView/app/src/main/java/com/orchsik/helloadapterview/MainActivity.kt
+++ HelloAdapterView/app/src/main/java/com/orchsik/helloadapterview/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.orchsik.helloword
+package com.orchsik.helloadapterview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.hello_button)
+        setContentView(R.layout.activity_main)
     }
-} 
+}
