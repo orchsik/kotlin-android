d5c5385c2c72fe08a8cfd3e20b9c7a6c5e8714e1
orchsik <orchsik@naver.com>
2021-03-17 22:23:44 +0900

    Add: HelloActivity Project

---
+++ HelloActivity/app/src/main/java/com/orchsik/helloactivity/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.orchsik.helloactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
